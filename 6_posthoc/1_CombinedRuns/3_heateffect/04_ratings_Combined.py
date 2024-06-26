7f802485310740833a0e14789d30d634e4048970
alicedabbagh <38045425+alicedabbagh@users.noreply.github.com>
2024-06-26 19:31:16 +0200

    Update 04_ratings_Combined.py

--- 6_posthoc/1_CombinedRuns/3_heateffect/04_ratings_Combined.py
+++ 6_posthoc/1_CombinedRuns/3_heateffect/04_ratings_Combined.py
@@ -24,7 +24,7 @@ data = []
 excluded_subjects = {3, 31}
 
 # Loop over subjects
-for subject in range(1, 5):
+for subject in range(1, 41):
     if subject in excluded_subjects:
         continue
     sub = f'sub-{str(subject).zfill(2)}'
